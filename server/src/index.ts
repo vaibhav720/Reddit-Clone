4081e94ef8dcf99065bc75052deaeb3499b32114
vaibhav720 <parikhvaibhav19@gmail.com>
2022-06-17 19:19:50 +0530

    micro orm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
+import { Post } from './entities/Post';
+import { __prod__ } from './constants';
+import {MikroORM} from "@mikro-orm/core"
+ 
+
+const main = async () =>{
+    const orm = await MikroORM.init({
+        entities:[Post],
+        dbName: "lireddit",
+        type: "postgresql",
+        debug: !__prod__
+    });
+
+    const post = orm.em.create(Post , {title: "this my first post"});
+
+    await orm.em.persistAndFlush(post);
+
+    await orm.em.nativeInsert(Post,{title:"my first post"});
+};
+
+
+main();
+
 console.log("Hello world");
